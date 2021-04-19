4d4fa4914a345c8815f6b21cfb5584d520bc8445
Jochem Vogel <voge0031@hz.nl>
2021-04-19 14:34:03 +0200

    Add match id check for chat

--- src/js/chat.js
+++ src/js/chat.js
@@ -2,16 +2,21 @@ chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let chatMessage = e.target[0].value;
+    let matchId = getMatchId()
 
     if (chatMessage !== '') {
-        socket.emit('chat-message', chatMessage)
+        socket.emit('chat-message', chatMessage, matchId)
         addMessage(chatMessage, true)
         e.target[0].value = ''
     }
 })
 
-socket.on("chat-message", (message) => {
-    addMessage(message, false)
+socket.on("chat-message", (message, socketMatchId) => {
+    const currentMatchId = getMatchId();
+
+    if (currentMatchId === socketMatchId) {
+        addMessage(message, false)
+    }
 })
 
 function addMessage(message, sender) {
@@ -25,3 +30,20 @@ function addMessage(message, sender) {
     messages.appendChild(newMessage);
     newMessage.scrollIntoView(true);
 }
+
+function getMatchId() {
+    const pathName = document.location.pathname;
+    const splittedArr = pathName.split('/');
+    return splittedArr[2];
+}
+
+
+// function toggleChat() {
+//     chatSection.classList.toggle('show');
+//     const matchIdFromUrl = getMatchId();
+//     socket.emit('join-room', matchIdFromUrl)
+// }
+
+// socket.on("join-room", (message) => {
+//     console.log('Room joined');
+// })
